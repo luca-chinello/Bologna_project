083f72f79fac5f16382f78675972163f396100d9
luca-chinello <luca.chinello@studenti.unimi.it>
2026-03-08 11:14:35 +0100

    data merging from qgis completed

--- 01-scripts/01-merging_qgis.R
+++ 01-scripts/01-merging_qgis.R
@@ -1,28 +1,54 @@
+rm(list = ls())
+
 library(sf)
 library(sfnetworks)
 library(dplyr)
 
-# 1. Loading the QGIS file, layer = "edges"
-edges_sf <- st_read("02-data/Bologna_Rete2.gpkg", layer = "edges")
+# 1. Loading the QGIS file
+nodes_sf <- st_read("02-data/Bologna_Rete_Finale.gpkg", layer = "nodes")
+edges_sf <- st_read("02-data/Bologna_Rete_Finale.gpkg", layer = "edges")
+
+names(nodes_sf)
 
 # 2. Creating the net from the existing edges
 net <- as_sfnetwork(edges_sf, directed = FALSE)
 
-# 3. Assigning IDs to nodes
-nodes_ID <- net %>% 
+net <- net %>% 
   activate("nodes") %>% 
-  st_as_sf()
+  st_join(nodes_sf, join = st_nearest_feature)
 
-# 4. Creating "from"and "to" columns for edges
-arches <- net %>% 
+# 3. Activating nodes and edges, dropping the geometry to let igraph work better
+nodes_for_igraph <- net %>% 
+  activate("nodes") %>% 
+  st_as_sf() %>% 
+  st_drop_geometry() %>%
+  mutate(node_id = row_number()) %>%
+  relocate(node_id) %>%
+  distinct(node_id, .keep_all = TRUE) %>%
+  select(where(~!all(is.na(.))))
+
+edges_for_igraph <- net %>% 
   activate("edges") %>% 
-  st_as_sf()
+  st_as_sf() %>% 
+  st_drop_geometry() %>% 
+  select(where(~!all(is.na(.))))
 
-# 5. Exporting the dataset, CSV and gpkg formats
-# CSV
-write.csv(st_drop_geometry(nodes_ID), "02-data/Bologna_Nodes_igraph.csv", row.names = FALSE)
-write.csv(st_drop_geometry(arches), "02-data/Bologna_Edges_igraph.csv", row.names = FALSE)
+# 5. Crating the graph
+bologna_graph <- graph_from_data_frame(
+  d = edges_for_igraph, 
+  vertices = nodes_for_igraph, 
+  directed = FALSE
+)
 
-# GeoPackage:
-st_write(nodes_ID, "02-data/Dataset_Bologna_Network.gpkg", layer = "nodes", append = FALSE)
-st_write(arches, "02-data/Dataset_Bologna_Network.gpkg", layer = "edges", append = FALSE)
+vertex_attr_names(bologna_graph)
+saveRDS(bologna_graph, file = "02-data/bologna_graph.rds")
+
+# -----------
+
+# Steps to read the data in the final script
+
+library(igraph)
+library(dplyr)
+
+bologna_graph <- readRDS("02-data/bologna_graph.rds")
+vertex_attr_names(grafo_bologna)
